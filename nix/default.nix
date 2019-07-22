945e00cdd02c4a32d9b4ee5814f76b39d6b3cd05
AmineChikhaoui <amine.chikhaoui91@gmail.com>
2019-07-22 16:40:49 -0400

    propagate pkgs instead of lib

--- nix/default.nix
+++ nix/default.nix
@@ -1,13 +1,13 @@
 {
-  config_exporters = { optionalAttrs, lib, ... }: [
+  config_exporters = { optionalAttrs, pkgs, ... }: with pkgs.lib; [
     (config :
     { virtualbox =
       optionalAttrs (config.deployment.targetEnv == "virtualbox")
         (config.deployment.virtualbox
         //
-        { disks = lib.mapAttrs (n: v: v //
+        { disks = mapAttrs (n: v: v //
           { baseImage =
-            if lib.isDerivation v.baseImage then "drv"
+            if isDerivation v.baseImage then "drv"
             else toString v.baseImage;
           }) config.deployment.virtualbox.disks;
         });

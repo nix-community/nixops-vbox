d3edf7ffdd232a9c8847b85cf6df6f2e9f634723
Graham Christensen <graham@grahamc.com>
2020-04-03 12:49:02 -0400

    switch to a poetry-based plugin

---
+++ nixops_vbox/plugin.py
@@ -0,0 +1,14 @@
+import os.path
+import nixops.plugins
+
+@nixops.plugins.hookimpl
+def nixexprs():
+    return [
+        os.path.dirname(os.path.abspath(__file__)) + "/nix"
+    ]
+
+@nixops.plugins.hookimpl
+def load():
+    return [
+        "nixops_vbox.backends.virtualbox",
+    ]

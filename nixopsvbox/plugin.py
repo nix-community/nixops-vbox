fd8ace2d47ad3c1f098f178e96b66fca6dc003dd
Graham Christensen <graham@grahamc.com>
2020-07-17 12:07:18 -0400

    nix exprs: relocate under nixopsvbox/

--- nixopsvbox/plugin.py
+++ nixopsvbox/plugin.py
@@ -6,17 +6,7 @@ from nixops.plugins import Plugin
 class NixopsVboxPlugin(Plugin):
     @staticmethod
     def nixexprs():
-        expr_path = os.path.realpath(
-            os.path.dirname(__file__) + "/../../../../share/nix/nixops-vbox"
-        )
-        if not os.path.exists(expr_path):
-            expr_path = os.path.realpath(
-                os.path.dirname(__file__) + "/../../../../../share/nix/nixops-vbox"
-            )
-        if not os.path.exists(expr_path):
-            expr_path = os.path.dirname(__file__) + "/../nix"
-
-        return [expr_path]
+        return [os.path.dirname(os.path.abspath(__file__)) + "/nix"]
 
     @staticmethod
     def load():
